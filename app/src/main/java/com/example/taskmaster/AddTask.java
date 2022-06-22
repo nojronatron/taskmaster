6f3dc036178ea2b70ac35d3ce5975bf2e39c6760
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-06-22 16:40:45 -0700

    completed implementing addtask page.

--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -3,12 +3,37 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AddTask extends AppCompatActivity {
+
+//    private int totalTaskCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+//        TextView totalTaskCount = AddTask.this.findViewById(R.id.totalTaskCount);
+//        totalTaskCount.setText(totalTaskCounter);
+
+        // Add event driven textbox change on button press
+        Button addThisTaskButton = AddTask.this.findViewById(R.id.addThisTaskButton);
+
+        addThisTaskButton.setOnClickListener(view -> {
+            Log.i("", "Entered incrementTaskCounter lambda.");
+//            incrementTaskCounter();
+//            setTaskCounterText();
+        });
     }
+
+//    private void incrementTaskCounter() {
+//        this.totalTaskCounter++;
+//    }
+
+//    private void setTaskCounterText() {
+//        TextView totalTaskCount = AddTask.this.findViewById(R.id.totalTaskCount);
+//        totalTaskCount.setText(totalTaskCounter);
+//    }
 }
