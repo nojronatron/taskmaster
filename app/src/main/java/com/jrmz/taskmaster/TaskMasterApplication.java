aaba34c79089e11ad1414acec6564864d4316b08
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-07-11 20:16:22 -0700

    ran amplify commands and updated gradle and App entrypoint to support S3

--- app/src/main/java/com/jrmz/taskmaster/TaskMasterApplication.java
+++ app/src/main/java/com/jrmz/taskmaster/TaskMasterApplication.java
@@ -7,6 +7,7 @@ import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 
 // this will be the new entrypoint for this app
 public class TaskMasterApplication extends Application {
@@ -18,6 +19,7 @@ public class TaskMasterApplication extends Application {
         try {
             Amplify.addPlugin(new AWSApiPlugin());
             Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSS3StoragePlugin());
             Amplify.configure(getApplicationContext());
             Log.i("MyAmplifyApp", "Initialized Amplify");
         } catch (AmplifyException ampe) {
